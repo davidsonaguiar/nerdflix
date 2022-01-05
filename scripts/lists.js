864529dabd5a7ac2284895b256a572f957d9adf9
Davidson <dra1@aluno.ifal.edu.br>
2022-01-05 00:30:40 -0300

    itens inicias adicionados

---
+++ scripts/lists.js
@@ -0,0 +1,98 @@
+let films = [
+    {
+        titulo: 'matrix',
+        diretor: 'lana wachowski',
+        studio: 'warner bros',
+        ulr: './img/matrix.jpg'
+    },
+    {
+        titulo: 'batman',
+        diretor: 'Christopher Nolan',
+        studio: 'warner bros',
+        ulr: './img/batman.jpg'
+    },
+    {
+        titulo: 'de volta para o futuro',
+        diretor: 'robert zemeckis',
+        studio: 'universal',
+        ulr: './img/devoltaparaofuturo.jpg'
+    },
+    {
+        titulo: 'free guy',
+        diretor: 'shawn levy',
+        studio: '20th century',
+        ulr: './img/freeguy.jpg'
+    },
+    {
+        titulo: 'capitao America',
+        diretor: 'joe russo',
+        studio: 'marvel',
+        ulr: './img/gerracivil.jpg'
+    },
+    {
+        titulo: 'hobbit',
+        diretor: 'peter jackson',
+        studio: 'warner bros',
+        ulr: './img/hobbit.jpg'
+    },
+    {
+        titulo: 'jhon wick',
+        diretor: 'chad ctahelski',
+        studio: 'thunder road pictures',
+        ulr: './img/jhonwick.jpg'
+    },
+    {
+        titulo: 'star wars',
+        diretor: 'george lucas',
+        studio: 'lucas films',
+        ulr: './img/starwars.jpg'
+    },
+    {
+        titulo: 'matrix',
+        diretor: 'lana wachowski',
+        studio: 'warner bros',
+        ulr: './img/matrix.jpg'
+    },
+    {
+        titulo: 'batman',
+        diretor: 'Christopher Nolan',
+        studio: 'warner bros',
+        ulr: './img/batman.jpg'
+    },
+    {
+        titulo: 'de volta para o futuro',
+        diretor: 'robert zemeckis',
+        studio: 'universal',
+        ulr: './img/devoltaparaofuturo.jpg'
+    },
+    {
+        titulo: 'free guy',
+        diretor: 'shawn levy',
+        studio: '20th century',
+        ulr: './img/freeguy.jpg'
+    },
+    {
+        titulo: 'capitao America',
+        diretor: 'joe russo',
+        studio: 'marvel',
+        ulr: './img/gerracivil.jpg'
+    },
+    {
+        titulo: 'hobbit',
+        diretor: 'peter jackson',
+        studio: 'warner bros',
+        ulr: './img/hobbit.jpg'
+    },
+    {
+        titulo: 'jhon wick',
+        diretor: 'chad ctahelski',
+        studio: 'thunder road pictures',
+        ulr: './img/jhonwick.jpg'
+    },
+    {
+        titulo: 'star wars',
+        diretor: 'george lucas',
+        studio: 'lucas films',
+        ulr: './img/starwars.jpg'
+    }
+];
